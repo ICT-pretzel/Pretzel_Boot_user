0ec135887a1d68acdb6e6961eb401570cfa22a21
wngud <ramyun1537@gmail.com>
2024-06-20 09:33:40 +0900

    0620주형푸쉬

--- src/main/java/com/ict/pretzel/jung/controller/TestController.java
+++ src/main/java/com/ict/pretzel/jung/controller/TestController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/test")
 public class TestController {
     
-	@GetMapping("/detail")
-	public String detail(@RequestParam("movie_id") String movie_id) {
+	@GetMapping("/test1")
+	public String test1(@RequestParam("keyword") String keyword) {
 		try {
-			String apiURL = "https://api.themoviedb.org/3/movie/"+movie_id+"?language=ko-kr&language=en-US";
+			String apiURL = "https://api.themoviedb.org/3/search/movie?query="+keyword;
 			String api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmE1OThkMzg4OTgwZjBlMTJjNmU1N2RkYjRmNjFlNyIsInN1YiI6IjY2NzEzMGNlNDA1YjNhMjk3MDZhYWFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cT8hOciOWfO-qUWSh_fzqQzVburxqSAqwdXoaTgHz1E";
 			URL url = new URL(apiURL);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -35,108 +35,6 @@ public class TestController {
 			if(responeseCode == HttpURLConnection.HTTP_OK) {
 				BufferedReader br =
 						new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-				
-				String line ="";
-				StringBuffer sb2 = new StringBuffer();
-				while((line=br.readLine()) !=null) {
-					sb2.append(line);
-				}
-				String result = sb2.toString();
-				System.out.println(result);
-				return result;
-			}
-			
-		} catch (Exception e) {
-			System.out.println("연결 실패");
-		}
-		return null;
-	}
-	@GetMapping("/credits")
-	public String credits(@RequestParam("movie_id") String movie_id) {
-		try {
-			String apiURL = "https://api.themoviedb.org/3/movie/"+movie_id+"/credits?language=ko-kr&language=en-US";
-			String api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmE1OThkMzg4OTgwZjBlMTJjNmU1N2RkYjRmNjFlNyIsInN1YiI6IjY2NzEzMGNlNDA1YjNhMjk3MDZhYWFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cT8hOciOWfO-qUWSh_fzqQzVburxqSAqwdXoaTgHz1E";
-			URL url = new URL(apiURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			
-			conn.setRequestMethod("GET");
-			
-			// 헤더 요청
-			conn.setRequestProperty("accept", "application/json");
-			conn.setRequestProperty("Authorization", "Bearer "+api_key);
-			int responeseCode = conn.getResponseCode();
-			System.out.println(responeseCode);
-			if(responeseCode == HttpURLConnection.HTTP_OK) {
-				BufferedReader br =
-						new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-				
-				String line ="";
-				StringBuffer sb2 = new StringBuffer();
-				while((line=br.readLine()) !=null) {
-					sb2.append(line);
-				}
-				String result = sb2.toString();
-				System.out.println(result);
-				return result;
-			}
-			
-		} catch (Exception e) {
-			System.out.println("연결 실패");
-		}
-		return null;
-	}
-	@GetMapping("/overview")
-	public String overview(@RequestParam("movie_id") String movie_id) {
-		try {
-			String apiURL = "https://api.themoviedb.org/3/movie/"+movie_id+"/translations?language=ko-kr&language=en-US";
-			String api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmE1OThkMzg4OTgwZjBlMTJjNmU1N2RkYjRmNjFlNyIsInN1YiI6IjY2NzEzMGNlNDA1YjNhMjk3MDZhYWFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cT8hOciOWfO-qUWSh_fzqQzVburxqSAqwdXoaTgHz1E";
-			URL url = new URL(apiURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			
-			conn.setRequestMethod("GET");
-			
-			// 헤더 요청
-			conn.setRequestProperty("accept", "application/json");
-			conn.setRequestProperty("Authorization", "Bearer "+api_key);
-			int responeseCode = conn.getResponseCode();
-			System.out.println(responeseCode);
-			if(responeseCode == HttpURLConnection.HTTP_OK) {
-				BufferedReader br =
-						new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-				
-				String line ="";
-				StringBuffer sb2 = new StringBuffer();
-				while((line=br.readLine()) !=null) {
-					sb2.append(line);
-				}
-				String result = sb2.toString();
-				System.out.println(result);
-				return result;
-			}
-			
-		} catch (Exception e) {
-			System.out.println("연결 실패");
-		}
-		return null;
-	}
-	@GetMapping("/trailer")
-	public String trailer(@RequestParam("movie_id") String movie_id) {
-		try {
-			String apiURL = "https://api.themoviedb.org/3/movie/"+movie_id+"/videos";
-			String api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmE1OThkMzg4OTgwZjBlMTJjNmU1N2RkYjRmNjFlNyIsInN1YiI6IjY2NzEzMGNlNDA1YjNhMjk3MDZhYWFlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cT8hOciOWfO-qUWSh_fzqQzVburxqSAqwdXoaTgHz1E";
-			URL url = new URL(apiURL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			
-			conn.setRequestMethod("GET");
-			
-			// 헤더 요청
-			conn.setRequestProperty("accept", "application/json");
-			conn.setRequestProperty("Authorization", "Bearer "+api_key);
-			int responeseCode = conn.getResponseCode();
-			System.out.println(responeseCode);
-			if(responeseCode == HttpURLConnection.HTTP_OK) {
-				BufferedReader br =
-						new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
 				
 				String line ="";
 				StringBuffer sb2 = new StringBuffer();
--- src/main/java/com/ict/pretzel/vo/MovieVO.java
+++ src/main/java/com/ict/pretzel/vo/MovieVO.java
@@ -1,11 +1,8 @@
 package com.ict.pretzel.vo;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.Data;
 
 @Data
 public class MovieVO {
-    private String movie_idx, title, thema, director, actor, synopsis, video_path, poster_path;
-    private MultipartFile video_file, poster_file;
+    private String movie_idx, movie_id, title, movie_src, thema, note;
 }
