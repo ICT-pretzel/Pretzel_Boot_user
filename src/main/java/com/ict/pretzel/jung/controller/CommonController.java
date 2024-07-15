c5976dcd9ddad47a75666afed0c008a2404d6861
choihyeonmin <minchoi9979@nave.com>
2024-07-15 20:28:06 +0900

    07-15-chm

--- src/main/java/com/ict/pretzel/jung/controller/CommonController.java
+++ src/main/java/com/ict/pretzel/jung/controller/CommonController.java
@@ -1,9 +1,5 @@
 package com.ict.pretzel.jung.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ict.pretzel.vo.MovieVO;
-
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -11,15 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/common")
 public class CommonController {
     @GetMapping("/image")
-    public ResponseEntity<?> image(@RequestParam String imageName) { 
+    public ResponseEntity<?> image(@RequestParam("imageName") String imageName) {     
     String path = "src\\main\\resources\\upload\\"; //이미지가 저장된 위치 
-    Resource resource = new FileSystemResource(path + imageName); 
+    Resource resource = new FileSystemResource(path + imageName);
         return new ResponseEntity<>(resource, HttpStatus.OK); 
     }
 }
